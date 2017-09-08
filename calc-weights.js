f39b4f57df20dcb9964fd886eedadd6ea0bef683
Walter Moore <wmoore@conversantmedia.com>
2017-09-08 12:16:28 -0500

    Added markdown output

--- calc-weights.js
+++ calc-weights.js
@@ -1,7 +1,8 @@
 var https = require('https');
 
-const PORTFOLIO_SIZE = process.argv[2];
-const PORTFOLIO_NOTIONAL = process.argv[3];
+const PORTFOLIO_SIZE = process.argv[2] || 25;
+const PORTFOLIO_NOTIONAL = process.argv[3] || 100000;
+const OUTPUT_FORMAT = process.argv[4] || 'cli';
 
 function getMarketCaps(callback) {
     return https.get('https://api.coinmarketcap.com/v1/ticker/', function(response) {
@@ -37,6 +38,15 @@ getMarketCaps(function(marketCaps){
         longestName = Math.max(longestName, mc.name.length);
     });
     console.log(`Total MarketCap: $${totalCap}`);
+    if(OUTPUT_FORMAT === 'markdown'){
+        renderMarkdown(mcs, totalCap);
+    }else{
+        renderCli(mcs, totalCap, longestName);
+    }
+});
+
+
+function renderCli(mcs, totalCap, longestName){
     // Calculate weight for each market:
     let longestUsd = 0;
     mcs.forEach(function(mc){
@@ -53,4 +63,20 @@ getMarketCaps(function(marketCaps){
             mc.market_cap_weight_cxy.toFixed(2) + " " + mc.symbol 
         );
     });
-});
+}
+
+function renderMarkdown(mcs, totalCap){
+    // Calculate weight for each market:
+    let longestUsd = 0;
+    console.log(`Coin | Weight | USD | Position`);
+    console.log(`-----|--------|-----|---------`);
+    mcs.forEach(function(mc){
+        mc.market_cap_weight = 100 * mc.market_cap_usd / totalCap;
+        mc.market_cap_weight_usd = mc.market_cap_weight * PORTFOLIO_NOTIONAL / 100;
+        mc.market_cap_weight_cxy = mc.market_cap_weight_usd / mc.price_usd;
+        if(longestUsd === 0){
+            longestUsd = mc.market_cap_weight_usd.toFixed(2).length;
+        }
+        console.log(`${mc.name} | %${mc.market_cap_weight.toFixed(2)} | $${mc.market_cap_weight_usd.toFixed(2)} | ${mc.symbol}`);
+    });
+}
